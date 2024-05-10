baf6a1443faae48c6400ff120424fec7b8faed38
EdricLai <edric.lai25@ycdsbk12.ca>
2024-05-10 12:43:04 -0400

    updated

--- Sketch.java
+++ Sketch.java
@@ -1,36 +1,168 @@
+// imports
 import processing.core.PApplet;
 
+/**
+ * Description: processing with inputs
+ * @author EdricLai
+*/
 public class Sketch extends PApplet {
-	
-	
+  /*
+   * global variables
+   * called once
+   */
+  boolean boolUp;
+  boolean boolDown;
+  boolean boolRight;
+  boolean boolLeft;
+  float fltScale;
+  float fltSpeed;
+  float fltPosX;
+  float fltPosY;
+  float fltDiameter;
+  float fltStrokeWeight;
+
   /**
-   * Called once at the beginning of execution, put your size all in this method
+   * settings function
+   * called once
    */
   public void settings() {
-	// put your size call here
-    size(400, 400);
+    size(1000, 700);
   }
 
   /** 
-   * Called once at the beginning of execution.  Add initial set up
-   * values here i.e background, stroke, fill etc.
+   * setup function
+   * called once
    */
   public void setup() {
-    background(210, 255, 173);
+    // general
+    background(0, 0, 0);
+
+    // initialize variables
+    fltPosX = width / 2;
+    fltPosY = height / 2;
+    fltScale = width * height;
+    fltSpeed = fltScale / 40000;
+    fltDiameter = fltScale / 20000;
+    fltStrokeWeight = fltScale / 200000;
   }
 
   /**
-   * Called repeatedly, anything drawn to the screen goes here
+   * draw function
+   * called repeatedly
    */
   public void draw() {
-	  
-	// sample code, delete this stuff
-    stroke(128);
-    line(150, 25, 270, 350);  
+    disintegrate();
+    noStroke();
+    blueAsteroid();
+    redAsteroid();
+  }
 
-    stroke(255);
-    line(50, 125, 70, 50);  
+  /**
+   * disintegration effect
+   * called on command
+   */
+  private void disintegrate() {
+    // customizes lines
+    stroke(0, 0, 0);
+    strokeWeight(fltStrokeWeight);
+
+    // erases visuals with random lines
+    for (int i = 0; i <= 30; i++) {
+      line(random(width), random(height), random(width), random(height));
+    }
+  }
+
+  /**
+   * blue asteroid code
+   * called on command
+   */
+  private void blueAsteroid() {
+    // called on mouse hold
+    if (mousePressed) {
+      // blue asteroid visual
+      fill(0, 255, 255);
+      ellipse(mouseX, mouseY, fltDiameter, fltDiameter);
+    }
+  }
+
+  /**
+   * mouse released function
+   * called on mouse release
+   */
+  public void mouseReleased() {
+    // blue explosion visual
+    fill(0, 100, 255);
+    ellipse(mouseX, mouseY, fltDiameter * 5, fltDiameter * 5);
+  }
+
+  /**
+   * red asteroid code
+   * called on command
+  */
+  private void redAsteroid() {
+    // called on keyboard hold
+    if (keyPressed) {
+      // red asteroid visual
+      fill(255, 0, 0);
+      ellipse(fltPosX, fltPosY, fltDiameter, fltDiameter);    
+    }
+
+    // movement
+    if (boolUp) {
+      fltPosY -= fltSpeed;
+    }
+    if (boolDown) {
+      fltPosY += fltSpeed;
+    }
+    if (boolRight) {
+      fltPosX += fltSpeed;
+    }
+    if (boolLeft) {
+      fltPosX -= fltSpeed;
+    }
+  }
+
+  /**
+   * keyboard pressed function
+   * called on keyboard pressed 
+  */
+  public void keyPressed() {
+    // wasd key pressed
+    if (key == 'w' || keyCode == UP) {
+      boolUp = true;
+    }
+    else if (key == 's' || keyCode == DOWN) {
+      boolDown = true;
+    }
+    else if (key == 'd' || keyCode == RIGHT) {
+      boolRight = true;
+    }
+    else if (key == 'a' || keyCode == LEFT) {
+      boolLeft = true;
+    }
   }
   
-  // define other methods down here.
+  /**
+   * keyboard released function
+   * called on keyboard release
+  */
+  public void keyReleased() {
+    // wasd key released
+    if (key == 'w' || keyCode == UP) {
+      boolUp = false;
+    }
+    else if (key == 's' || keyCode == DOWN) {
+      boolDown = false;
+    }
+    else if (key == 'd' || keyCode == RIGHT) {
+      boolRight = false;
+    }
+    else if (key == 'a' || keyCode == LEFT) {
+      boolLeft = false;
+    }
+
+    // red explosion visual
+    fill(255, 100, 0);
+    ellipse(fltPosX, fltPosY, fltDiameter * 3, fltDiameter * 3);
+  }
 }
